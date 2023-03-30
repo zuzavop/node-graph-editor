debe00a4968cbbd3964f3a95f0ee30d17334814a
Vopálková Zuzana <935-vopalkoz@users.noreply.localhost>
2023-03-30 12:13:59 +0000

    update drawing nodes

--- src/node.cpp
+++ src/node.cpp
@@ -2,40 +2,15 @@
 #include "edge.h"
 
 void Node::draw(SDL_Renderer* renderer, int radius) const {
-   const int32_t diameter = (radius * 2);
-
-   int32_t rx = (radius - 1);
-   int32_t ry = 0;
-   int32_t tx = 1;
-   int32_t ty = 1;
-   int32_t error = (tx - diameter);
-
-   while (rx >= ry)
-   {
-      //  Each of the following renders an octant of the circle
-      SDL_RenderDrawPoint(renderer, x + rx, y - ry);
-      SDL_RenderDrawPoint(renderer, x + rx, y + ry);
-      SDL_RenderDrawPoint(renderer, x - rx, y - ry);
-      SDL_RenderDrawPoint(renderer, x - rx, y + ry);
-      SDL_RenderDrawPoint(renderer, x + ry, y - rx);
-      SDL_RenderDrawPoint(renderer, x + ry, y + rx);
-      SDL_RenderDrawPoint(renderer, x - ry, y - rx);
-      SDL_RenderDrawPoint(renderer, x - ry, y + rx);
-
-      if (error <= 0)
-      {
-         ++ry;
-         error += ty;
-         ty += 2;
-      }
-
-      if (error > 0)
-      {
-         --rx;
-         tx += 2;
-         error += (tx - diameter);
-      }
-   }
+   for (int i = 0; i < radius * 2; i++) {
+        for (int j = 0; j < radius * 2; j++) {
+            int dx = radius - i;
+            int dy = radius - j;
+            if ((dx*dx + dy*dy) <= (radius * radius)) {
+                SDL_RenderDrawPoint(renderer, x + dx, y + dy);
+            }
+        }
+    }
 }
     
 void Node::addEdge(std::shared_ptr<Edge> edge) {
